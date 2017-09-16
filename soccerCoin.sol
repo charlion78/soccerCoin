fe5cc0caaaa41dac7b9274e556f7203119467ff0
charlion78 <charlion78@gmail.com>
2017-09-16 11:50:29 +0200

    Create soccerCoin.sol

---
+++ soccerCoin.sol
@@ -0,0 +1,13 @@
+import "./StandardToken.sol";
+pragma solidity ^0.4.13;
+
+// implementing soccerCoin as ERC223 token standard
+
+contract soccerCoin is StandardToken {
+string public constant name = "soccerCoin";
+ string public constant symbol = "soC";
+ uint8 public constant decimals = 18;
+ 
+ string public constant name = "soccerCoin";
+ string public constant symbol = "soC";
+ uint8 public constant decimals = 18;
